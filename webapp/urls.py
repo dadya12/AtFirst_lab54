898421bba43cf129cc4fdd331605813d834ef589
Adam <adam.nemmish213@gmail.com>
2024-05-06 00:26:17 +0600

    Реализовал создание категории

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from webapp.views import home, detail
+from webapp.views import home, detail, add_category
 
 urlpatterns = [
     path('', home, name='home'),
     path('products/', home, name='home_oke'),
     path('products/<int:pk>/', detail, name='detail'),
+    path('categories/add/', add_category, name='add_category'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from webapp.models import Category, Product
 
 
@@ -10,3 +10,14 @@ def home(request):
 def detail(request, *args, pk, **kwargs):
     products = get_object_or_404(Product, pk=pk)
     return render(request, 'product_view.html', {'products': products})
+
+
+def add_category(request):
+    if request.method == 'GET':
+        return render(request, 'category_add_view.html')
+    elif request.method == "POST":
+        category = Category.objects.create(
+            title=request.POST.get('title'),
+            description=request.POST.get('description'),
+        )
+        return redirect('home')
