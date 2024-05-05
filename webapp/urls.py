d6f90417304786b057694c1f53c73e97956b47b6
Adam <adam.nemmish213@gmail.com>
2024-05-05 23:30:11 +0600

    Добавил детальный просмотр продукта так же добавил навигационные ссылки в нав бар

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from webapp.views import home
+from webapp.views import home, detail
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('products/', home, name='home_oke'),
+    path('products/<int:pk>/', detail, name='detail'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from webapp.models import Category, Product
 
 
 def home(request):
     products = Product.objects.all()
     return render(request, 'products_view.html', {'products': products})
+
+
+def detail(request, *args, pk, **kwargs):
+    products = get_object_or_404(Product, pk=pk)
+    return render(request, 'product_view.html', {'products': products})
